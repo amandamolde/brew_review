0280852494ebc0bd4976692d72332f7cc0dfa6f8
amandamolde <amanda.molde@gmail.com>
2018-10-25 11:06:29 -0600

    Installed and required body-parser

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Set up middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(9000, () => {
     console.log('API is listening on port 9000');
